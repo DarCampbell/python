50825c8d13e8169694c9a86f0c46894a57ee8887
Darlisha Campbell <40171398+DarCampbell@users.noreply.github.com>
2020-12-17 16:34:19 -0500

    Add files via upload

---
+++ introduce_app.py
@@ -0,0 +1,21 @@
+#This app will ask classmates their name and a few questiosn about themselves.
+#Afterward, the app will share the answers given by the classmates
+
+#Greeting
+print('Welcome back to school! Answer these 3 questions to introduce yourself!')
+
+#Question 1
+name = input('What is your name? ')
+
+#Question 2
+favorite_color = input('What is your favorite color? ')
+
+#Question 3
+favorite_food = input('What is your favorite food? ')
+
+#Question 4
+favorite_tvShow = input('What is your favorite TV show? ')
+
+
+#Greeting to the class
+print(f"Everyone, meet {name}! {name}'s favorite color is {favorite_color}. {name}'s favorite food is {favorite_food}. {name}'s favorite TV show is {favorite_tvShow}.")
