bd8d3f914fb2e7bd3784d7c4c565318f88bc2153
Darlisha Campbell <40171398+DarCampbell@users.noreply.github.com>
2023-01-09 15:36:12 -0500

    Update calculator2.py

--- calculator2.py
+++ calculator2.py
@@ -1,9 +1,11 @@
+#create function called main that prints the sqaured value of the user input
 def main():
     x = int(input("What's X? "))
     print("X sqaured is", square(x))
 
+#create a square function that squares 2 numbers
 def square(n):
     return n * n
 
-
+# Calls function main
 main()
